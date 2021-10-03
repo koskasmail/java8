4267b66897c61d62df9d07f2bd814c8addcb8905
kessler yaron <koskasmail@gmail.com>
2021-10-03 18:40:34 +0300

    🚧 findFirst() element   🚧
    findFirst() -  element that meet the condition

--- 02-Lambda_Expression/lambadaIn/Lambda_06_find_first_element_in_stream.java
+++ 02-Lambda_Expression/lambadaIn/Lambda_06_find_first_element_in_stream.java
@@ -3,13 +3,29 @@
 public class Lambda_06_find_first_element_in_stream 
 {
 	public Lambda_06_find_first_element_in_stream() 
-	{	
+	{
 		ProductsInventory p6 = new ProductsInventory();
+
 		System.out.println("Product info with id is p2");
-		p6.getStatMainList().stream()
+
+//		output to String
+		String out = p6.getStatMainList()
+				.stream()
+				.filter(p -> p.getId().equalsIgnoreCase("id-1001"))
+				.findFirst()
+				.toString();
+
+//		output to object "Product"
+		Product intOut = p6.getStatMainList()
+				.stream()
 				.filter(p -> p.getId().equalsIgnoreCase("id-1001"))
 				.findFirst()
 				.get();
-		System.out.println(p6.getStatMainList());
+
+// output
+		System.out.println(out);
+		System.out.println("-----------------------------------------------");
+		System.out.println(intOut);
 	}
 }
+	
